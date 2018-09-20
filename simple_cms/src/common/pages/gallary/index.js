0fffbec93dc19caf29ac394fad7c392139bba7f3
Jason Qu <fishinsea2014@gmail.com>
2018-09-20 15:33:21 +1200

    Create frame of gallery.

---
+++ simple_cms/src/common/pages/gallary/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Row,Col,Card,Collapse} from 'antd';
+import b1 from '../../images/minren.jpg';
+import b2 from '../../images/zouzu.jpg';
+
+const Palel= Collapse.Panel;
+
+class Album extends Component{
+    render(){
+        return(
+            <div>this is gallery</div>
+        );
+    }
+}
+
+export default Album;
